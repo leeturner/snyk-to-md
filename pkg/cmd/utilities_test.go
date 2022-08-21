41e0c9f3d5b494c0b18f285ceb69a8894cc927d6
leeturner <lee@leeturner.me>
2022-08-21 19:28:43 +0100

    Now we have refactored the utilities class we need to rename a few of the fields

--- pkg/cmd/utilities_test.go
+++ pkg/cmd/utilities_test.go
@@ -11,29 +11,29 @@ func TestGetFileContent(t *testing.T) {
 	log := zaptest.NewLogger(t)
 	logger := *log.Sugar()
 	tests := []struct {
-		testName    string
-		jsonInput   string
-		expectedMD  string
-		expectedErr error
+		testName      string
+		jsonInputFile string
+		expectedJson  string
+		expectedErr   error
 	}{
 		{
-			testName:    "input file does not exist",
-			jsonInput:   "./doesnt-exist.txt",
-			expectedMD:  "",
-			expectedErr: errors.New("could not open the file"),
+			testName:      "input file does not exist",
+			jsonInputFile: "./doesnt-exist.txt",
+			expectedJson:  "",
+			expectedErr:   errors.New("could not open the file"),
 		},
 		{
-			testName:    "input file does exist",
-			jsonInput:   "../../test-data/dummy-report.json",
-			expectedMD:  `{"ok": false}`,
-			expectedErr: nil,
+			testName:      "input file does exist",
+			jsonInputFile: "../../test-data/dummy-report.json",
+			expectedJson:  `{"ok": false}`,
+			expectedErr:   nil,
 		},
 	}
 
 	for _, data := range tests {
 		t.Run(data.testName, func(t *testing.T) {
-			resultMD, resultErr := getFileContent(data.jsonInput, logger)
-			assert.Equal(t, data.expectedMD, resultMD)
+			resultMD, resultErr := getFileContent(data.jsonInputFile, logger)
+			assert.Equal(t, data.expectedJson, resultMD)
 			assert.Equal(t, data.expectedErr, resultErr)
 		})
 	}
